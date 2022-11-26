66f65459749b3f9f7ad6a3ebb726624788149574
Michel Grootjans <michel.grootjans@gmail.com>
2022-11-26 10:11:49 +0100

    test builders

--- frontend/src/components/footer/footer.test.tsx
+++ frontend/src/components/footer/footer.test.tsx
@@ -1,6 +1,21 @@
 import { countTodosLeft, Footer } from "./index";
 import { screen, render } from "../../test-lib/test-utils";
+import {aTodo} from "../../test-lib/todo";
 
-it("dummy test", () => {
-    expect(1 + 1).toEqual(2);
+it("empty array", () => {
+    expect(countTodosLeft([])).toEqual(0);
+});
+
+it("not done", () => {
+    expect(countTodosLeft([
+        aTodo({done: false}),
+    ])).toEqual(1);
+});
+
+it("all done", () => {
+    expect(countTodosLeft([
+        aTodo({done: true}),
+        aTodo({done: true}),
+        aTodo({done: true}),
+    ])).toEqual(0);
 });
---
+++ frontend/src/test-lib/todo.ts
@@ -0,0 +1,7 @@
+import {TodoTO} from "../lib/api";
+import {faker} from "@faker-js/faker";
+
+export function aTodo(overrides: Partial<TodoTO>) {
+    return {id: faker.datatype.uuid(), description: faker.lorem.text(), done: faker.datatype.boolean(), ...overrides};
+}
+
